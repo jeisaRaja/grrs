5ed5374f88d8e579f59923cf2e958d92561be060
jeisaRaja <jeisa.raja.jrt@gmail.com>
2024-08-11 19:41:26 +0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use core::panic ;
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    let result = std::fs::read_to_string(&args.path);
+    let content = match result {
+        Ok(content) => content,
+        Err(err) => {
+            panic!("Can't deal with {}, just exit here", err)
+        }
+    };
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line)
+        }
+    }
+}
